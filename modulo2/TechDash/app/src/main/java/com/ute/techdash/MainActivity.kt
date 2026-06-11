11af6520a321232216ca4a722f1f2494b565d906
llazo1995 <llazo@ecargo.ec>
2026-06-10 19:58:42 -0500

    clase 10/06/2026

--- modulo2/TechDash/app/src/main/java/com/ute/techdash/MainActivity.kt
+++ modulo2/TechDash/app/src/main/java/com/ute/techdash/MainActivity.kt
@@ -4,7 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.ute.techdash.ui.multimedia.PantallaMultimedia
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ute.techdash.ui.hardware.DashboardHardware
+import com.ute.techdash.ui.hardware.PantallaMenu
+import com.ute.techdash.ui.hardware.gps.PantallaGPS
+import com.ute.techdash.ui.hardware.sensores.PantallaSensores
 import com.ute.techdash.ui.theme.TechDashTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +19,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TechDashTheme {
-                PantallaMultimedia()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "menu") {
+                    composable("menu")      { PantallaMenu(navController) }
+                    composable("gps")       { PantallaGPS() }
+                    composable("sensores")  { PantallaSensores() }
+                    composable("dashboard") { DashboardHardware() }
+                }
             }
         }
     }
